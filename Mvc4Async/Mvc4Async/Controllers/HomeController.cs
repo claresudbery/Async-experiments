a70a180330785cb95e0c950a1e36028d9635969c
SCN\clare.sudbery <clare.sudbery@thoughtworks.gov.uk>
2017-05-03 17:06:32 +0100

    Added code to experiment with concurrency.

--- Mvc4Async/Mvc4Async/Controllers/HomeController.cs
+++ Mvc4Async/Mvc4Async/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using Mvc4Async.Models;
 using System.Threading.Tasks;
@@ -32,6 +33,10 @@ namespace Mvc4Async.Controllers
                gizmoTask.Result
                );
 
+            ViewBag.numGizmos = pwgVM.gizmoList.Count();
+            ViewBag.numWidgets = pwgVM.widgetList.Count();
+            ViewBag.numProducts = pwgVM.prodList.Count();
+
             return View("PWG", pwgVM);
         }
 
@@ -57,6 +62,10 @@ namespace Mvc4Async.Controllers
                gizmoTask.Result
                );
 
+            ViewBag.numGizmos = pwgVM.gizmoList.Count();
+            ViewBag.numWidgets = pwgVM.widgetList.Count();
+            ViewBag.numProducts = pwgVM.prodList.Count();
+
             return View("PWG", pwgVM);
         }
 
@@ -73,22 +82,33 @@ namespace Mvc4Async.Controllers
                 gizmoService.GetGizmos()
                );
 
+            ViewBag.numGizmos = pwgVM.gizmoList.Count();
+            ViewBag.numWidgets = pwgVM.widgetList.Count();
+            ViewBag.numProducts = pwgVM.prodList.Count();
+
             return View("PWG", pwgVM);
         }
 
         public async Task<ActionResult> WidgetsAsync()
         {
+            System.Diagnostics.Debug.WriteLine("Entering WidgetsAsync. Context: " + SynchronizationContext.Current.GetHashCode());
             ViewBag.SyncOrAsync = "Asynchronous";
             var widgetService = new WidgetService();
-            return View("Widgets", await widgetService.GetWidgetsAsync());
+            var widgets = await widgetService.GetWidgetsAsync();
+            ViewBag.numWidgets = widgets.Count();
+            System.Diagnostics.Debug.WriteLine("Leaving WidgetsAsync");
+
+            return View("Widgets", widgets);
         }
 
         public ActionResult Widgets()
         {
             ViewBag.SyncOrAsync = "Synchronous";
             var widgetService = new WidgetService();
+            var widgets = widgetService.GetWidgets();
+            ViewBag.numWidgets = widgets.Count();
 
-            return View("Widgets", widgetService.GetWidgets());
+            return View("Widgets", widgets);
         }
 
         [AsyncTimeout(150)]
@@ -99,36 +119,55 @@ namespace Mvc4Async.Controllers
         {
             ViewBag.SyncOrAsync = "Asynchronous";
             var gizmoService = new GizmoService();
-            return View("Gizmos",
-                await gizmoService.GetGizmosAsync(cancellationToken));
+            var gizmos = await gizmoService.GetGizmosAsync(cancellationToken);
+            ViewBag.numGizmos = gizmos.Count();
+
+            return View("Gizmos", gizmos);
         }
 
         public async Task<ActionResult> GizmosAsync()
         {
+            System.Diagnostics.Debug.WriteLine("Entering GizmosAsync. Context: " +
+                                               SynchronizationContext.Current.GetHashCode());
             ViewBag.SyncOrAsync = "Asynchronous";
             var gizmoService = new GizmoService();
-            return View("Gizmos", await gizmoService.GetGizmosAsync());
+            var gizmos = await gizmoService.GetGizmosAsync();
+            ViewBag.numGizmos = gizmos.Count();
+            System.Diagnostics.Debug.WriteLine("Leaving GizmosAsync");
+
+            return View("Gizmos", gizmos);
         }
 
         public ActionResult Gizmos()
         {
             ViewBag.SyncOrAsync = "Synchronous";
             var gizmoService = new GizmoService();
-            return View("Gizmos", gizmoService.GetGizmos());
+            var gizmos = gizmoService.GetGizmos();
+            ViewBag.numGizmos = gizmos.Count();
+
+            return View("Gizmos", gizmos);
         }
 
         public async Task<ActionResult> ProductsAsync()
         {
+            System.Diagnostics.Debug.WriteLine("Entering ProductsAsync. Context: " + SynchronizationContext.Current.GetHashCode());
             ViewBag.SyncOrAsync = "Asynchronous";
             var productsService = new ProductService();
-            return View("Products", await productsService.GetProductsAsync());
+            var products = await productsService.GetProductsAsync();
+            ViewBag.numProducts = products.Count();
+            System.Diagnostics.Debug.WriteLine("Leaving ProductsAsync");
+
+            return View("Products", products);
         }
 
         public ActionResult Products()
         {
             ViewBag.SyncOrAsync = "Synchronous";
             var prodService = new ProductService();
-            return View("Products", prodService.GetProducts());
+            var products = prodService.GetProducts();
+            ViewBag.numProducts = products.Count();
+
+            return View("Products", products);
         }
 
         public async Task<ActionResult> NestedAsyncCallsNoWait()
